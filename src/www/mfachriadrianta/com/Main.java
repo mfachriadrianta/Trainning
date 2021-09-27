46ead36ab8ee7d73a78aab309658e03f04554d83
mfachriadrianta <muhammadfachriadrianta12@gmail.com>
2021-09-27 09:52:14 +0700

    add

--- src/www/mfachriadrianta/com/Main.java
+++ src/www/mfachriadrianta/com/Main.java
@@ -1,23 +1,30 @@
 package www.mfachriadrianta.com;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-        int a = 115;
+//        int a = 115;
+//
+//        long a_long = a;
+//        System.out.println(a_long);
+//
+//        byte a_byte = (byte) a;
+//        System.out.println(a_byte);
+//
+//        float b_float = 2;
+//
+//        float resultValue = a + b_float;
+//        System.out.printf("%d + %f = %f\n", a,b_float,resultValue);
+//
+//        String c_string = "Value";
+//        System.out.printf("%s = %f" , c_string, resultValue);
 
-        long a_long = a;
-        System.out.println(a_long);
+        parsingString();
+    }
 
-        byte a_byte = (byte) a;
-        System.out.println(a_byte);
-
-        float b_float = 2;
-
-        float resultValue = a + b_float;
-        System.out.printf("%d + %f = %f\n", a,b_float,resultValue);
-
-        String c_string = "Value";
-        System.out.printf("%s = %f" , c_string, resultValue);
+    public static void parsingString(){
 
     }
 }
